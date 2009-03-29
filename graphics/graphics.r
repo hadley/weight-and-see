71f1e0f50113df39387e56a05d10b9854070d719
hadley <h.wickham@gmail.com>
2009-03-29 12:02:18 -0500

    Changes for Antony

--- graphics/graphics.r
+++ graphics/graphics.r
@@ -1,4 +1,6 @@
-library(ggplot2)
+l(ggplot)
+l(plyr)
+theme_set(theme_bw())
 
 # Figure 1 -----------------------
 # UCB berkeley admissions
@@ -14,21 +16,32 @@ ggsave("berkeley.pdf", width = 6, height = 4)
 midwest <- read.csv("midwest.csv")
 base <- ggplot(midwest, aes(percblack)) + scale_x_continuous("Percent black")
 
-base + geom_histogram(binwidth = 1) +
-  scale_y_continuous("Counties")
+label <- function(text, y) {
+  list(
+    scale_y_continuous(text),
+    annotate("text", 40, y, label = text, hjust = 1)
+  )
+}
+
+base + geom_histogram(binwidth = 2) + 
+  scale_y_continuous("Counties") + 
+  annotate("text", 45, 300, label = "Counties", hjust = 1)
 ggsave("perc-black-count.pdf", width = 6, height = 4)
-base + geom_histogram(aes(weight = poptotal / 1e6), binwidth = 1) + 
-  scale_y_continuous("Population (millions)")
+
+base + geom_histogram(aes(weight = poptotal / 1e6), binwidth = 2) + 
+  scale_y_continuous("Population (millions)") + 
+  annotate("text", 45, 13, label = "Population", hjust = 1)
 ggsave("perc-black-pop.pdf", width = 6, height = 4)
-base + geom_histogram(aes(weight = popblack / 1e6), binwidth = 1) + 
-  scale_y_continuous("Black population (millions)")
+
+base + geom_histogram(aes(weight = popblack / 1e6), binwidth = 2) + 
+  scale_y_continuous("Black population (millions)") + 
+  annotate("text", 45, 1.8, label = "Black population", hjust = 1)
 ggsave("perc-black-pop-black.pdf", width = 6, height = 4)
-base + geom_histogram(aes(weight = popwhite / 1e6), binwidth = 1) + 
-  scale_y_continuous("White population (millions)")
+
+base + geom_histogram(aes(weight = popwhite / 1e6), binwidth = 2) + 
+  scale_y_continuous("White population (millions)") + 
+  annotate("text", 45, 13, label = "White population", hjust = 1)
 ggsave("perc-black-pop-white.pdf", width = 6, height = 4)
-
-
-qplot(percblack, data=midwest, geom="histogram", binwidth=1, weight=white.population / 1e6) + ylab("White population (millions)") + xlab
 
 # Figure 3 -------------------------
 # scatterplot of % change vs % share, + size proportional to population
@@ -36,7 +49,8 @@ qplot(percblack, data=midwest, geom="histogram", binwidth=1, weight=white.popula
 florida <- read.table("florida-election.txt", header=T)
 base <- ggplot(florida, aes(b00pc, b_change)) + 
   xlab("Percent of vote (2000)") + 
-  ylab("Change in percent of vote")
+  ylab("Change in percent of vote") +
+  xlim(0.275, 0.8)
 base + geom_point()  
 ggsave("florida.pdf", width = 4, height = 4)
 
@@ -50,8 +64,8 @@ ggsave("florida-weighted.pdf", width = 5, height = 4)
 
 keywords <- read.table("keywords-day.txt", header=TRUE, sep="\t")
 keywords$day <- as.Date(strptime(keywords$day, "%Y-%m-%d"))
-keywords$colour <- factor(ifelse(keywords$keyword == "AnzeigenKeyword9", "red", "grey60"), levels=c("red", "grey60"))
-keywords$company <- factor(ifelse(keywords$keyword == "bigX", "red", "grey60"), levels=c("red", "grey60"))
+keywords$colour <- factor(ifelse(keywords$keyword == "AnzeigenKeyword9", "red", "grey20"), levels=c("red", "grey20"))
+keywords$company <- factor(ifelse(keywords$keyword == "bigX", "red", "grey20"), levels=c("red", "grey20"))
 
 
 keyplot <- ggplot(keywords, aes(day)) + xlab(NULL) + 
@@ -64,14 +78,14 @@ ggsave("web-words.pdf", width = 6, height = 4)
 last_plot() + aes(fill = colour) + scale_fill_identity()
 ggsave("web-words-brushed.pdf", width = 6, height = 4)
 
-keyplot + aes(weight = clicks, fill = company) + 
+keyplot + aes(weight = clicks) + 
   scale_y_continuous("Clicks") + 
   scale_fill_identity()
 ggsave("web-clicks.pdf", width = 6, height = 4)
 
 stackplot <- plot_clone(last_plot())
 stackplot$layers[[1]]$position <- position_fill()
-stackplot + scale_y_continuous("Proportion of clicks")
+stackplot + scale_y_continuous("Proportion of clicks") + aes(fill = company)
 ggsave("web-clicks-stacked.pdf", width = 6, height = 4)
 
 # Figure 6 -------------------------
@@ -94,10 +108,10 @@ ggsave("post-prob.pdf", width = 8, height = 3)
 meta <- read.table("meta-analysis.txt", header=TRUE)
 qplot(w, data=meta, geom="histogram", binwidth=0.1) + xlab("Weight") + 
   scale_y_continuous("Count")
-ggsave("meta.pdf", width = 6, height = 4)
+ggsave("meta.pdf", width = 4, height = 4)
 last_plot() + aes(weight = w) + 
     scale_y_continuous("Weight")
-ggsave("meta-weighted.pdf", width = 6, height = 4)
+ggsave("meta-weighted.pdf", width = 4, height = 4)
 
 
 # Figure 9 -------------------------
